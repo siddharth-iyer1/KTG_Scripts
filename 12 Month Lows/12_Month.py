8a4e1188f210b7bd4898f6c718a0e0e323581b6b
Siddharth Iyer <86328355+siddharth-iyer1@users.noreply.github.com>
2022-06-29 13:52:15 -0500

    12_Month_Lows Final

--- 12 Month Lows/12_Month.py
+++ 12 Month Lows/12_Month.py
@@ -10,81 +10,50 @@ class Gr8Script7069f6af08d2470b966bea08b69990be(Strategy):
         SP500 = service.symbol_list.in_list(service.symbol_list.get_handle(SP500guid),symbol)
 
         return SP500 and md.stat.prev_close > 20 and md.stat.avol > 1000000
-        
-    # @classmethod
-    # def backtesting_extra_symbols(cls, symbol, md, service, account):
-    #     return ['SPY']
             
     def on_start(self, md, order, service, account):
-        # print(md.symbol)
-        
         bars = md.bar.daily(start = -264, end = -1)
         bars_low = bars.low
-        if(len(bars.low) >= 250):
+        if(len(bars.low) >= 250):                                              # Need at least 250 days of bar data
             low = min(bars.low)
-            # Console Print for backtest
-            # print('The 12-month low is ' + str(low))
-            # print('---------------------------------')
-            
-            self.qualified = ((md.stat.prev_close - low)/low)                                      # Amount stock is trading above low
-            self.low = low
-            print(self.low)
-            print(self.qualified)
+            self.qualified = ((md.stat.prev_close - low)/low)                  # Amount stock is trading above low
+            self.low = low                                                     # 12 Month Low   
         else:
             self.qualified = 0
             
-        self.traded = 0
-        self.alerted_at_2 = 0
+        self.traded = 0                                                        # Whether a stock has been traded or not
+        self.alerted_at_2 = 0                                                  # Whether a stock has alerted at 2% or not
         
-        service.add_time_trigger(service.time(15, 30))
+        service.add_time_trigger(service.time(15, 30))                         # Cover at 3:30 EST
             
-    # alert at 2% and 5 days before was new low
+    # Alert at 2%
     
     def on_trade(self, event, md, order, service, account):
-        if(account[self.symbol].position.shares == 0 and self.qualified >= 0.025):                  # Must be new position and trading 4% above low
+        if(account[self.symbol].position.shares == 0 and self.qualified >= 0.025):                  # Must be new position and trading >2.5% above low
                                                                                                     # on start of trade day
             if(md.L1.last <= 1.02*self.low and md.L1.last > self.low and self.alerted_at_2 == 0):   # Stock has broken 2% barrier and alert hasn't fired
-                _alertList = [('Price', md.L1.last), ('Message', str(self.symbol) + ' has reached 2% of 12 Month Low of ' + str(self.low))]
+                _alertList = [('Price', md.L1.last), ('Message', str(self.symbol) + ' 2% of 12 Month Low of ' + str(self.low) + ', Time: ' + service.time_to_string(service.system_time, format='%H:%M:%S'))]
                 service.alert( md.symbol, 'cf5a34c1-b6e5-4017-a316-cc652da9460f', _alertList )
-                self.alerted_at_2 = 1
+                self.alerted_at_2 = 1                                                               # Stock has alerted
                         
             if(md.L1.last <= self.low and self.traded == 0):                                        # Upon making a new low, short
-                _alertList = [('Price', md.L1.last), ('Message', 'New 12 Month Low, Shorted ' + str(self.symbol))]
+                _alertList = [('Price', md.L1.last), ('Message', 'New 12 Month Low, Time: ' + service.time_to_string(service.system_time, format='%H:%M:%S'))]
                 service.alert( md.symbol, '2785af4f-d521-4d26-b826-50c19f40e6cc', _alertList )
-                num_shares = round((1000/(md.L1.last*0.05)))                                        # Equal risk for all stocks
-                                                                                                    # 1000 dollar risk given 5% stop
+                num_shares = round((200/(1.03*(md.L1.last) - md.L1.last)))                          # Equal risk for all stocks
+                                                                                                    # 200 dollar risk given 3% stop
                 order.algo_sell(self.symbol, algorithm='8fdee8fe-b772-46bd-b411-5544f7a0d917', intent='init', order_quantity=num_shares)
-                self.traded = 1                                                                     # Prevents auto shorting if trader decides to
+                self.new_low = md.L1.last
+                self.traded = 1                                                                     # Stock has been traded
+                                                                                                    # Prevents auto shorting if trader decides to
                                                                                                     # manually exit
-            
-            if(account[self.symbol].position.shares != 0):
-                if(md.L1.last > 1.05*(account[self.symbol].position.entry_price)):
+                                                                                                    
+            if(account[self.symbol].position.shares != 0):                                          # Stop at 2% above entry
+                if(md.L1.last > 1.02*(account[self.symbol].position.entry_price)):
                     order.algo_buy(self.symbol, algorithm='2b4fdc55-ff01-416e-a5ea-e1f1d4524c7d', intent='exit')
             
-    # Exit at market close
+    # Exit at 3:30 EST
+    
     def on_timer(self, event, md, order, service, account):
         if(account[self.symbol].position.shares != 0):
             order.algo_buy(self.symbol, algorithm='market', intent='exit')
 
-    # Scalp/stop - For testing, going to use exit at market close
-
-        # Scalp after 3% profit or stop out after 3% loss
-        # if(account[self.symbol].position.shares != 0):
-            # if(md.L1.last > 1.03*(account[self.symbol].position.entry_price) or                    # Stopped out
-              # md.L1.last < 0.97*(account[self.symbol].position.entry_price)):                      # Scalp
-                # order.algo_buy(self.symbol, algorithm='2b4fdc55-ff01-416e-a5ea-e1f1d4524c7d', intent='exit')
-           
-            
-            #----------------------- Sell Init-----------------------
-            # trade_data = {}
-            # orderShares = abs(<shares>)
-            # orderPrice = 0.0
-            # VoodooGuid = '{'8fdee8fe-b772-46bd-b411-5544f7a0d917'  #Sell Market ARCA}'
-            # order.algo_sell( md.symbol, voodooGuid, 'init', order_quantity=orderShares, price=orderPrice, allow_multiple_pending=False, user_key=None, collect=trade_data )
-
-            #----------------------- Buy Exit-----------------------
-            # trade_data = {}
-            # orderPrice = 0.0
-            # voodooGuid = '{'2b4fdc55-ff01-416e-a5ea-e1f1d4524c7d'  #Buy Market ARCA}'
-            # order.algo_buy( md.symbol, voodooGuid, 'exit', price=orderPrice, allow_multiple_pending=False, user_key=None, collect=trade_data )
-            
